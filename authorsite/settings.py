9732f67b499bf8a54c31fe8d17dbc521142b52be
ireneuszcierpisz <>
2024-01-28 15:37:16 +0000

    Set environment variables.

--- authorsite/settings.py
+++ authorsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$dosaulyin5l)pp(3etrfysk#gp+1u(euh66_6e=9pmk@n@-y-'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
