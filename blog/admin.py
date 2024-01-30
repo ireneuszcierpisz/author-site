ca7c42e7bc89c2154346bfc3154826765e1b50d0
ireneuszcierpisz <>
2024-01-30 12:53:36 +0000

    Apdate the blog admin to use summernote.

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+	list_display = ('title', 'slug', 'status')
+	search_fields = ['title']
+	list_filter = ('status',)
+	prepopulated_fields = {'slug': ('title',)}
+	summernote_fields = ('content',)
