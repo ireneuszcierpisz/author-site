791e5f0bdd67ae6e3cc120cdbff6c5adb55d94a3
ireneuszcierpisz <>
2024-02-04 12:50:24 +0000

    Handle the contact form POST request and save the submited data to the database.

--- contact/views.py
+++ contact/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import Contact
 from .forms import ContactForm
 
 
 def contact_me(request):
     contact = Contact.objects.all().order_by('-updated_on').first()
+    if request.method == "POST":
+        contact_form = ContactForm(data=request.POST)
+        if contact_form.is_valid:
+            contact_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'I received your message! I will try to respond within three days.'
+            )
     contact_form = ContactForm()
 
     return render(
